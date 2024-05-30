feea718d8801777708626faaddfeb7c67247317a
Louie Casula <121182711+louiecasula@users.noreply.github.com>
2024-05-30 14:02:47 -0400

    Added to Greeting statement and styling.

--- src/pages/Greeting.js
+++ src/pages/Greeting.js
@@ -1,14 +1,14 @@
 import React from 'react';
-// import './Greeting.css';
+import './Greeting.css';
 import LouieHeadshot from '../images/LouieHeadshot.JPG';
 
 function Greeting() {
     return (
         <>
             <h1>Hi, I'm Louie Casula</h1>
-            <div className="about-block">
-                <img className="about-image" src={LouieHeadshot} alt="Louie's headshot"/>
-                <p>Here is a long paragraph about how I became a programmer. </p>
+            <div className="greeting-block">
+                <img className="greeting-image" src={LouieHeadshot} alt="Louie's headshot"/>
+                <p>an aspiring full-stack software developer working out of the greater Philadelphia area</p>
             </div>
         </>
     )
