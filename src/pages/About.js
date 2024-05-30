aa6268963fb687ebeb5ba4652b803821a19ab8f8
Louie Casula <121182711+louiecasula@users.noreply.github.com>
2024-05-30 14:21:08 -0400

    Filled out some of the About paragraph.

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,7 +7,11 @@ function About() {
             <hr/>
             <h1 id="about">About Me</h1>
             <div className="about-block">
-                <p>Here is a long paragraph about how I became a programmer. </p>
+                <p>
+                    During my senior year in undergrad I took my first programming course out of vague curiosity and a free slot in my schedule.
+                    Fast-forward two years later and writing code has become a staple in my everyday life, whether doing daily LeetCode problems or learning how to create an idea that came to me in the shower.
+                    After all of the time I've spent studying, there is still an entire world left to discover and I'm excited to stumble upon that next educational endeavor.
+                </p>
             </div>
         </>
     )
