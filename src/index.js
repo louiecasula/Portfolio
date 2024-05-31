b709c6833c7388bb27a072665e41f8ceeb80fd1c
Louie Casula <121182711+louiecasula@users.noreply.github.com>
2024-05-31 11:47:32 -0400

    Made app runnable for development and production.

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const basename = process.env.NODE_ENV === 'production' ? '/Portfolio' : '';
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/Portfolio">
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
