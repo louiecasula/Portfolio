24cd05c8cc98370959b827af85a29546f287074b
Louie Casula <121182711+louiecasula@users.noreply.github.com>
2024-06-04 12:37:27 -0400

    Re-styled Greeting page.

--- src/pages/Greeting.js
+++ src/pages/Greeting.js
@@ -7,8 +7,8 @@ function Greeting() {
         <div className="greeting-block">
             <img className="greeting-image" src={LouieHeadshot} alt="Louie's headshot"/>
             <div className="text-container">
-                <h1>Hi, I'm Louie Casula</h1>
-                <p>an aspiring full-stack software developer looking for work in the Greater Philadelphia area</p>
+                <h1>Hi, I'm <span id="name">Louie</span>.</h1>
+                <p>I'm an aspiring <b>full stack software developer</b> looking for work in the Greater Philadelphia area.</p>
             </div>
         </div>
     )
