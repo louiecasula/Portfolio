1517ce2a18c4691b836e5cb41f62e63f6ce99a94
Louie Casula <121182711+louiecasula@users.noreply.github.com>
2024-06-17 12:23:15 -0400

    Added contact form and styling.

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import './Contact.css';
+import './Contact.css';
 
 function Contact() {
     return (
@@ -7,9 +7,14 @@ function Contact() {
             <hr/>
             <h1>Let's Connect!</h1>
             <div className="about-block text-container">
-                <p>
-                    Feel free to reach out to me at any of the links below :)
-                </p>
+                <form action="https://formspree.io/f/mvoeeyvr" method="POST">
+                    <div class="input-group">
+                        <input type="text" name="name" placeholder="Your Name" required />
+                        <input type="email" name="email" placeholder="Your Email" required />
+                    </div>
+                    <textarea name="message" placeholder="Message" required></textarea>
+                    <button type="submit">Send</button>
+                </form>
             </div>
         </div>
     )
